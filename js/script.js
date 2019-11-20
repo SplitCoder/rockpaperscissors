53a7a9d6b9513268d50e21cfb71cb7cad55c1148
Alan Ramirez <43681186+SplitCoder@users.noreply.github.com>
2019-11-20 18:22:39 -0500

    worked up till the computer choice array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $("#shoot").click(function(){
    let UserWord = $("#input").val();
-   $("#userChoice").text(UserWord)
+   $("#userChoice").text(UserWord);
         let RandomNum = Math.floor((Math.random() * 3) + 0);
-        let CompChoices = ["scissors", "rock", "paper"]
+        let CompChoices = ["scissors", "rock", "paper"];
+});
 
 
 
