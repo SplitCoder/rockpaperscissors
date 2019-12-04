2fcc31e6ab1f9303b523a1b2af813401da35c323
Alan Ramirez <aramirez2@hudsonhs.com>
2019-12-04 23:23:11 +0000

    just need to   add the results an project is done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $("#shoot").click(function(){
-   let UserWord = $("#input").val();
-   $("#userChoice").text(UserWord);
-        let RandomNum = Math.floor((Math.random() * 3) + 0);
-        let CompChoices = ["scissors", "rock", "paper"];
-});
+    let UserWord = $("#input").val();
+        $("#userChoice").text(UserWord);
+    let RandomNum = Math.floor((Math.random() * 3) + 0);
+    let CompChoices = ["scissors", "rock", "paper"];    
+    let compInput = CompChoices[RandomNum];
+        $("#computerChoice").text(compInput);
 
-
-
+    if
+        ((UserWord === "scissors") && compInput === "rock"){
+        $("#result").text("Computer Wins!");
+        }else if
+        ((UserWord === "scissors") && compInput === "scissors"){
+        $("#result").text("No One Wins...");
+        }else if 
+        ((UserWord === "scissors") && compInput === "paper"){
+        $("#result").text("You Win!");
+        }
 
 
 
