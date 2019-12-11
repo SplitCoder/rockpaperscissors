9cb520af701c6faf2fada7d133aae82560324e2b
Alan Ramirez <aramirez2@hudsonhs.com>
2019-12-11 22:54:52 +0000

    finished projectgit add .

--- js/script.js
+++ js/script.js
@@ -5,56 +5,43 @@ $("#shoot").click(function(){
     let CompChoices = ["scissors", "rock", "paper"];    
     let compInput = CompChoices[RandomNum];
         $("#computerChoice").text(compInput);
-
+debugger;
     if
         ((UserWord === "scissors") && compInput === "rock"){
         $("#result").text("Computer Wins!");
+        $("body").css("background", "#ff0000");
         }else if
         ((UserWord === "scissors") && compInput === "scissors"){
         $("#result").text("No One Wins...");
+        $("body").css("background", "#849181");
         }else if 
         ((UserWord === "scissors") && compInput === "paper"){
         $("#result").text("You Win!");
+        $("body").css("background", "#2bff00");
+        }else if 
+        ((UserWord === "rock") && compInput === "paper"){
+        $("#result").text("Computer Wins!");
+        $("body").css("background", "#ff0000");
+        }else if 
+        ((UserWord === "rock") && compInput === "rock"){
+        $("#result").text("No One Wins...");
+        $("body").css("background", "#849181");
+        }else if 
+        ((UserWord === "rock") && compInput === "scissors"){
+        $("#result").text("You Win!");
+        $("body").css("background", "#2bff00");
+        }else if 
+        ((UserWord === "paper") && compInput === "scissors"){
+        $("#result").text("Computer Wins!");
+        $("body").css("background", "#ff0000");
+        }else if 
+        ((UserWord === "paper") && compInput === "paper"){
+        $("#result").text("No One Wins...");
+        $("body").css("background", "#849181");
+        }else if
+        ((UserWord === "paper") && compInput === "rock"){
+        $("#result").text("You Win!");
+        $("body").css("background", "#2bff00");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
